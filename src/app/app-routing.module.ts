ba8f048e8f3c2bd9188b363020566ca2e257c8e3
mb-suhailc <suhail.c@mindbowser.com>
2022-05-19 18:21:11 +0530

    corrections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActivateHomeProductsGuard } from './auth/activate-home-products.guard';
+import { Err404Component } from './error/err404/err404.component';
+import { HomeComponent } from './login-home/home/home.component';
+import { LoginComponent } from './login-home/login/login.component';
+import { ProductsComponent } from './user-products/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [ActivateHomeProductsGuard]
+  },
+  {
+    path: 'products',
+    component: ProductsComponent,
+    canActivate: [ActivateHomeProductsGuard],
+    loadChildren: () => import('./user-products/user-products.module').then(m => m.UserProductsModule)
+  },
+  {
+    path: '**',
+    component: Err404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login-home/login-home-routing.module.ts
+++ src/app/login-home/login-home-routing.module.ts
@@ -1,32 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ActivateHomeProductsGuard } from '../auth/activate-home-products.guard';
-import { Err404Component } from '../error/err404/err404.component';
-import { ProductsComponent } from '../user-products/products/products.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: LoginComponent
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-    canActivate: [ActivateHomeProductsGuard]
-  },
-  {
-    path: 'products',
-    component: ProductsComponent,
-    canActivate: [ActivateHomeProductsGuard],
-    loadChildren: () => import('../user-products/user-products.module').then(m => m.UserProductsModule)
-  },
-  {
-    path: '**',
-    component: Err404Component
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
